e30e80fd9af663273ed7d7d5cadcbd7039930b9b
spacew <107980615+Universenn@users.noreply.github.com>
2023-04-06 17:59:16 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -3,23 +3,13 @@ class Solution {
         int answer = 0;
         int bigger = sides[0] > sides[1] ? sides[0] : sides[1];
         int smaller = sides[0] > sides[1] ? sides[1] : sides[0];
-
-        int a = 0;
-        // 첫번째 제일 큰수가 sides 에 있을때 같아도 안됨
-        // bigger(6)  < smaller(3) + (4,5,6)
-        for (int i = 0; i <= bigger; i++) {
+        for (int i = 0; i < bigger+smaller; i++) {
             if (bigger < smaller + i) {
-                System.out.println(i);
                 answer++;
-            }
-        }
-
-        // 미지의 수가 제일 큰 수 일때
-        // ?6<(7,8)<9 < bigger(6) + smaller(3)
-        for (int i = bigger; i <= bigger+smaller; i++) {
-            if (bigger < i && i < bigger+smaller) {
-                System.out.println(i);
-                answer++;
+            }else{
+             if (bigger < i && i < bigger+smaller) {
+                    answer++;
+               }
             }
         }
 
