70103edbed4344c0916c51fe579e9a1bc792bea6
spacew <107980615+Universenn@users.noreply.github.com>
2023-04-23 18:08:59 +0900

    [level 0] Title: 배열 자르기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int x = 0;
+        int[] answer = new int[num2-num1+1];
+        for(int i = num1; i <= num2; i++){
+
+            answer[x] = numbers[i];
+            x++;
+        }
+        return answer;
+    }
+}
