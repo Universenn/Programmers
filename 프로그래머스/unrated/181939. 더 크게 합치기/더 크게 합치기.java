56b756b503a3ad2359bc089f583edbf34be283b3
spacew <107980615+Universenn@users.noreply.github.com>
2023-04-27 15:12:25 +0900

    [unrated] Title: 더 크게 합치기, Time: 7.57 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        int result1 = Integer.parseInt("" + a + b);
+        int result2 = Integer.parseInt("" + b + a);;
+        
+        // int answer = result1 > result2 ? result1 : result2;
+        
+        return result1 > result2 ? result1 : result2;
+    }
+}
