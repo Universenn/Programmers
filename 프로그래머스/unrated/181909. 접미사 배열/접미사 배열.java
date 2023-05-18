eb5f7c0ef9fdd471aada12d3834d4985808f2bc9
spacew <107980615+Universenn@users.noreply.github.com>
2023-05-18 12:05:15 +0900

    [unrated] Title: 접미사 배열, Time: 0.22 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -1,12 +1,6 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
-        for(int i = my_string.length()-1; i >= 0; i--){
-            answer[i] = my_string.substring(i);
-        }
-        Arrays.sort(answer);
+        String[] answer = {};
         return answer;
     }
 }
