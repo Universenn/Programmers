3c2c4379a6e77cf32d1a0258f8db9f4d4092f037
spacew <107980615+Universenn@users.noreply.github.com>
2023-06-05 10:34:17 +0900

    [unrated] Title: 배열 만들기 3, Time: 6.80 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,16 +1,17 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        int[] answer = new int[intervals[0][1]- intervals[0][0] + intervals[1][1] - intervals[1][0] + 2];
-        int temp1 = intervals[0][0];
-        int temp2 = intervals[1][0];
-        for(int i = 0; i < intervals[0][1]- intervals[0][0]+1; i++){
-            answer[i] = arr[temp1];
-            temp1++;
+//        int[] answer = {};
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = intervals[0][0]; i <= intervals[0][1]; i++) {
+            list.add(arr[i]);
         }
-        for(int i = intervals[0][1]- intervals[0][0]+1; i < intervals[0][1]- intervals[0][0] + intervals[1][1] - intervals[1][0] + 2; i++){
-            answer[i] = arr[temp2];
-            temp2++;
+        for (int i = intervals[1][0]; i <= intervals[1][1]; i++) {
+            list.add(arr[i]);
         }
+        int[] answer = list.stream().mapToInt(i->i).toArray();
         return answer;
     }
 }
