25f1c8c363fbbde474701c24eec58822ca136f00
spacew <107980615+Universenn@users.noreply.github.com>
2023-06-05 10:38:10 +0900

    [unrated] Title: 배열 만들기 3, Time: 1.30 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,17 +1,24 @@
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
 //        int[] answer = {};
-        LinkedList<Integer> list = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = intervals[0][0]; i <= intervals[0][1]; i++) {
             list.add(arr[i]);
         }
         for (int i = intervals[1][0]; i <= intervals[1][1]; i++) {
             list.add(arr[i]);
         }
-        int[] answer = list.stream().mapToInt(i->i).toArray();
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+//        int[] answer = list.stream().mapToInt(i->i).toArray();
+//        System.out.println(Arrays.toString(answer));
         return answer;
     }
 }
