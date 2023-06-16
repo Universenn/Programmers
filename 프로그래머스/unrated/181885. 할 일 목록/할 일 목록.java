11be342841d0bcea3491bfda39989ce759d5b52b
spacew <107980615+Universenn@users.noreply.github.com>
2023-06-16 21:32:52 +0900

    [unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+
+        ArrayList<String> arr = new ArrayList<>();
+        for(int i = 0; i < finished.length; i++){
+            if(!finished[i]){
+                arr.add(todo_list[i]);
+            }
+        }
+        
+        String[] answer = new String[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i); 
+        }
+        
+        
+        return answer;
+    }
+}
