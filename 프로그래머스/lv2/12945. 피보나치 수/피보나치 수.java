716af6fef94ffc32f89f4dbf3d678238ba64d091
spacew <107980615+Universenn@users.noreply.github.com>
2023-09-27 15:09:18 +0900

    [level 2] Title: 피보나치 수, Time: 2.46 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+       int[] cache = new int[n+1];
+        
+        cache[0] = 0;
+        cache[1] = 1;
+        
+        for (int i=2; i<= n; i++){
+            cache[i] = (cache[i-1] + cache[i-2]) % 1234567;
+        }
+        
+        return cache[n] % 1234567;
+    }
+}
