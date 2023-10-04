856985741f4b28490a15350402f0a9fa7c5d323d
spacew <107980615+Universenn@users.noreply.github.com>
2023-10-04 14:17:10 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -8,10 +8,12 @@ class Solution {
         return answer;
     }
     private int gcd(int a, int b){
-        if(b == 0){
+        int tempA = Math.max(a, b);
+        int tempB = Math.min(a, b);
+        if(tempB == 0){
             return a;
         }
-        return gcd(b, a % b);
+        return gcd(tempB, tempA % tempB);
     }
     private int lcm(int a, int b){
         return a * b/ gcd(a, b);
