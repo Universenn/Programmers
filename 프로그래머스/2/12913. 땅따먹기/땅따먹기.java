a1d6708827f46c167700beefd9173bdda1311b28
spacew <107980615+Universenn@users.noreply.github.com>
2023-10-18 11:24:08 +0900

    [level 2] Title: 땅따먹기, Time: 26.45 ms, Memory: 85.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,22 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int dp[][] = new int[land.length + 1][4];
+
+        for(int i = 1; i <= land.length; i++){
+            for(int j = 0; j < 4; j++){
+                for(int k = 0; k < 4; k++){
+                    if(k == j){
+                        continue;
+                    }
+
+                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][k]);
+
+                    answer = Math.max(answer, dp[i][j]);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
