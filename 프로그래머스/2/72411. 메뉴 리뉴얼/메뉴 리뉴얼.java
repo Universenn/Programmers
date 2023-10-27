fd135cd78053352240a9f67a2664bf3f0de40cce
spacew <107980615+Universenn@users.noreply.github.com>
2023-10-27 12:46:04 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 31.29 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,53 +1,51 @@
 import java.util.*;
 class Solution {
-    
+
     HashMap<String, Integer> hashMap = new HashMap<>();
-    ArrayList<String> answerList = new ArrayList<>();
+    ArrayList<String> arrayList = new ArrayList<>();
+
     public String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
+
         // orders 정렬
         for (int i = 0; i < orders.length; i++) {
             char[] chars = orders[i].toCharArray();
             Arrays.sort(chars);
             orders[i] = String.valueOf(chars);
         }
-
-        // course 별로 조합 만들기
-        for (int courseLength : course) {
-            for (String order : orders)
-                combination("", order, courseLength);
-                // 가장 많은 조합을 answer 에 저장
-            if (!hashMap.isEmpty()) {
-                List<Integer> countList = new ArrayList<>(hashMap.values());
-                int max = Collections.max(countList);
-
-                if (max > 1)
-                    for (String key : hashMap.keySet())
-                        if (hashMap.get(key) == max)
-                            answerList.add(key);
-                hashMap.clear();
+        
+        // course 수만큼 order 정렬
+        for (int count : course) {
+            for (String order : orders) {
+                combination("", order, count);
             }
-
+            if (!hashMap.isEmpty()) {
+                List<Integer> list = new ArrayList<>(hashMap.values());
+                int max = Collections.max(list);
+                if (max > 1 ){
+                    for (String key : hashMap.keySet()) {
+                        if (hashMap.get(key) == max) {
+                            arrayList.add(key);
+                        }
+                    }
+                }
+            }
+            hashMap.clear();
         }
-        Collections.sort(answerList);
-        answer = new String[answerList.size()];
+        Collections.sort(arrayList);
+        String[] answer = new String[arrayList.size()];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = answerList.get(i);
+            answer[i] = arrayList.get(i);
         }
         return answer;
     }
 
-    private void combination(String order, String orders, int courseLength) {
-        
-        // 탈출 조건
-        if (courseLength == 0) {
+    void combination(String order, String orders, int count) {
+        if (count == 0) {
             hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
             return;
         }
-        // 수행 동작
         for (int i = 0; i < orders.length(); i++) {
-            combination(order + orders.charAt(i), orders.substring(i + 1), courseLength - 1);
+            combination(order + orders.charAt(i), orders.substring(i + 1), count - 1);
         }
     }
-
 }
