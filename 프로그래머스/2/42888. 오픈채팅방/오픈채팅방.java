969008bfa2160e01e4323d8ac5bfa070be4383ae
spacew <107980615+Universenn@users.noreply.github.com>
2023-11-11 16:41:13 +0900

    [level 2] Title: 오픈채팅방, Time: 98.53 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] record) {
+        // String[] answer = new int[record.length];
+        String[] answer = {};
+        ArrayList<String> list = new ArrayList<>();
+        HashMap<String, String> map = new HashMap<>();
+        for(String str : record){
+            String[] arr = str.split(" ");
+            if(arr[0].equals("Enter")){
+                map.put(arr[1], arr[2]);
+            }else if(arr[0].equals("Change")){
+                map.put(arr[1], arr[2]);
+            }
+        }
+        
+        for(int i = 0; i < record.length; i++){
+            String[] arr = record[i].split(" ");
+            if(arr[0].equals("Enter")){
+                list.add(map.get(arr[1]) + "님이 들어왔습니다.");
+            }else if(arr[0].equals("Leave")){
+                list.add(map.get(arr[1]) + "님이 나갔습니다.");
+            }
+        }
+        
+        // System.out.println(list);
+        return list.toArray(new String[0]);
+    }
+}
