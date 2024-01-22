06d2c5015847ee5e0f5448bda1ee7490292681c7
spacew <107980615+Universenn@users.noreply.github.com>
2024-01-22 15:36:29 +0900

    [level 1] Title: 폰켓몬, Time: 6.61 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,13 +1,15 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Set<Integer> set = Arrays.stream(nums)
-                .boxed()
-                .collect(Collectors.toSet());
-        
-        return set.size() > (nums.length / 2) ? nums.length / 2 : set.size();
+        int answer = nums.length/2;
+        Set<Integer> numSet = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            numSet.add(nums[i]);
+        }
+        if(numSet.size() <= nums.length/2){
+            answer = numSet.size();
+        }
+        return answer;
     }
 }
