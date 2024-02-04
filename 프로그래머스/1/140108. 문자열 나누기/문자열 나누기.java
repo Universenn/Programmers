bec032ffc9c4ad5e1fa29d95aab99b0405ddee8e
spacew <107980615+Universenn@users.noreply.github.com>
2024-02-04 16:08:36 +0900

    [level 1] Title: 문자열 나누기, Time: 3.72 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int count = 0;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            
+            if(stack.isEmpty()){
+                stack.push(c);
+            }
+            
+            count = stack.peek() == c ? count + 1 : count - 1; 
+            
+            if(count == 0){
+                answer++;
+                stack.clear();
+            }
+        }
+        
+        return stack.size() == 0 ? answer : answer + 1;
+    }
+}
