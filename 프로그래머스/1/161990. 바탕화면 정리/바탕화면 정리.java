8501446f65b45f090e528d13e15d35ab05788391
spacew <107980615+Universenn@users.noreply.github.com>
2024-04-01 20:18:10 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,40 +1,22 @@
 class Solution {
-    public int[] solution(String[] wallpaper){
-        int[] answer = new int[4];
-
-        // lux
+    public int[] solution(String[] wallpaper) {
+        int[] answer = {};
+        int lux = 50;
+        int luy = 0;
+        int rux = 50;
+        int ruy = 0;
+        
         for(int i = 0; i < wallpaper.length; i++){
-            if(wallpaper[i].contains("#")){
-                answer[0] = i;
-                break;
-            }
-        }
-
-        // rdx
-        for(int i = wallpaper.length-1; i >= 0; i--){
-            if(wallpaper[i].contains("#")){
-                answer[2] = i+1;
-                break;
-            }
-        }
-
-        // luy rdy
-        answer[1] = 100;
-        answer[3] = 0;
-
-        for(int i = 0; i < wallpaper.length; i++){
-            int index = wallpaper[i].indexOf("#");
-            if (index != -1) {
-                answer[1] = Math.min(answer[1], index);
-                int indexMax = wallpaper[i].lastIndexOf("#");
-                if (index != indexMax) {
-                    answer[3] = Math.max(answer[3], indexMax+1);
-                } else {
-                    answer[3] = Math.max(answer[3], index+1);
+            for(int j = 0; j < wallpaper[0].length(); j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    lux = Math.min(lux, i);
+                    luy = Math.max(luy, i+1);
+                    rux = Math.min(rux, j);
+                    ruy = Math.max(ruy, j+1);
                 }
             }
         }
-
-        return answer;
+        
+        return new int[]{lux, rux, luy, ruy};
     }
 }
