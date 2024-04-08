9010cc0027d15da8653b9d0a8a140ee29fe41fed
spacew <107980615+Universenn@users.noreply.github.com>
2024-04-08 13:36:46 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.41 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,22 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-        Map<String, Integer> map = new HashMap<>();
+        int[] answer = new int[]{0,0};
+        
         Stack<Character> stack = new Stack<>();
+        Map<String, Integer> map = new HashMap<>();
         int i = 0;
         for(String word : words){
-            char cF = word.charAt(0);
-            char cL = word.charAt(word.length()-1);
-            i++;
-            map.put(word, map.getOrDefault(word, 0) + 1);
-            if(map.get(word) == 2 || !stack.isEmpty() && stack.peek() != cF) {
+            i++;            
+            if(!stack.isEmpty() && stack.peek() == word.charAt(0)){
+                stack.pop();
+            }
+            char c = word.charAt(word.length()-1);
+            stack.push(c);
+            map.put(word, map.getOrDefault(word,0) +1);
+            
+            if(stack.size() > 1 || map.get(word) > 1){
                 answer[0] = i % n == 0 ? n : i % n;
-                answer[1] = i % n == 0 ? i / n : i / n  + 1;
+                answer[1] = (i + n - 1) / n;
                 break;
-            } 
-            stack.push(cL);
+            }
         }
+        
+
+
         return answer;
     }
 }
