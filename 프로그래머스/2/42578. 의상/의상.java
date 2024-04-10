3b1395b810a7619f63f20d01c4959ed0e5ddc25b
spacew <107980615+Universenn@users.noreply.github.com>
2024-04-10 23:37:49 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        return answer;
+        int answer = 1;
+        HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            ArrayList<String> list = hashMap.getOrDefault(clothes[i][1], new ArrayList<String>());
+            list.add(clothes[i][0]);
+            hashMap.put(clothes[i][1], list);
+        }
+
+        for (String s : hashMap.keySet()) {
+            ArrayList<String> list = hashMap.get(s);
+            answer *= (list.size() + 1);
+        }
+        
+        return answer-1;
     }
 }
