d51a23eaa103bce967fc72d93a2ff1025039a45a
spacew <107980615+Universenn@users.noreply.github.com>
2024-04-11 19:18:10 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1392.65 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,37 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
+         Set<Integer> set = new HashSet<>();
 
-        int[] elements2 = new int[elements.length * 2];
-
-        for (int i = 0; i < elements.length; i++) {
-            elements2[i] = elements[i];
-            elements2[elements.length + i] = elements[i];
-        }
-
-        Set<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < elements.length; i++) {
-            int sum = 0;
-            int temp = 0;
-            int temp2 = 0;
-            for (int j = 0; j < elements.length + i; j++) {
-
-                if (i == 0) {
-                    set.add(elements2[j]);
-                }else{
-                    sum += elements2[j];
-                    temp++;
-                    if (temp == i + 1) {
-                        temp2++;
-                        set.add(sum);
-                        sum = elements2[temp2];
-                        j = temp2;
-                        temp = 1;
-                    }
+        for (int i=1; i<=elements.length; i++) {
+            for (int j=0; j<elements.length; j++) {
+                int sum = 0;
+                for (int k=j; k<j+i; k++) {
+                    sum += elements[k%elements.length];
                 }
+                set.add(sum);
             }
         }
+
         return set.size();
     }
 }
